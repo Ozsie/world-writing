6fb5da5ab9ff0bb146c0487e5bc12ea59ed05a9c
Oscar Djupfeldt <oscar.djupfeldt@abilia.se>
2026-01-21 22:05:08 +0100

    add jan 21 world building

--- pages.js
+++ pages.js
@@ -2,5 +2,6 @@
 // Add more paths here manually if auto-discovery is not possible on your host.
 const PAGES = [
   { path: "world-building/January-12-2026.html", title: "January 12, 2026" },
-  { path: "world-building/January-14-2026.html", title: "January 14, 2026" }
+  { path: "world-building/January-14-2026.html", title: "January 14, 2026" },
+  { path: "world-building/January-21-2026.html", title: "January 21, 2026" }
 ];
